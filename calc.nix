37337914565aad892b2d120c8262985b9447d19a
Eric Lynema <elynema@gmail.com>
2024-05-04 13:56:48 -0700

    added nix support

---
+++ calc.nix
@@ -0,0 +1,21 @@
+let
+  pkgs = import <nixpkgs> { };
+in
+  pkgs.stdenv.mkDerivation {
+    name = "hello-nix";
+    
+    buildInputs = [
+      pkgs.nasm
+    ];
+
+    src = ./.;
+
+    buildPhase = ''
+      make asm
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp calc  $out/bin/calc
+    '';
+  }
